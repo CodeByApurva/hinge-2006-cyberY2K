193f67cd5c91025c78d8fa3271b4ae1f83f3894a
Apurva Jaiswal <apurvajaiswal2103@gmail.com>
2026-07-12 01:39:26 +0530

    Fix indentation and formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
   base: '/hinge-2006-cyberY2K/',
-
   plugins: [react()],
-
   server: {
-
     port: 3000,
-
     open: true,
-
-  },
-
+    },
 });
