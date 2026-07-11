4f9da5d598fd19849390d44e0a3043cfbdcc29ac
Apurva Jaiswal <apurvajaiswal2103@gmail.com>
2026-07-12 01:42:33 +0530

    Change BrowserRouter to HashRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { AppProvider } from './contexts/AppContext';
 import { ThemeProvider } from './contexts/ThemeContext';
