82e0617b12f76cca63fd788bec2d0b404f05c76d
Apurva Jaiswal <apurvajaiswal2103@gmail.com>
2026-07-12 01:43:45 +0530

    Replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
     <ThemeProvider>
       <AuthProvider>
         <AppProvider>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               {/* Guest Routes */}
               <Route 
@@ -64,7 +64,7 @@ export default function App() {
               {/* Catch-all Redirect */}
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </AppProvider>
       </AuthProvider>
     </ThemeProvider>
